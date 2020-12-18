b13181087a1b61d9ed7f1b163fb7f49bd2319f5e
Hi-Ayush <ayush.kumar.ee20@heritageit.edu.in>
2020-12-18 14:19:49 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -12,7 +12,37 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // your code goes here
+app.get("/mario",(req,res)=>{
+    marioModel.find().then((output)=>res.send(output));
+})
+
+app.get("/mario/:id",(req,res)=>{
+    const id=req.params.id;
+    marioModel.findById({_id:id}).then((output)=>res.send(output)).catch((err)=>{res.status(400).send({message: err.message})});
+})
+app.post("/mario",(req,res)=>{
+    const {name,weight}=req.body;
+    const newMarioCharacter=new marioModel({
+        name,
+        weight
+    });
+    newMarioCharacter.save().then((newMarioCharacter)=>res.status(201).send(newMarioCharacter)).catch((err)=>{res.status(400).send({message: 'either name or weight is missing'})});
+})
 
 
+app.patch("/mario/:id", (req, res) => {
+  const id = req.params.id;
+  marioModel
+    .findByIdAndUpdate(id, req.body, { new: true })
+    .then((mario) => res.send(mario))
+    .catch((err) => {res.status(400).send({ 
+        message: err.message 
+    })});
+});
+
+app.delete("/mario/:id",(req,res)=>{
+    const id=req.params.id;
+    marioModel.findByIdAndDelete({_id:id}).then((result)=>{res.status(200).send({message: 'character deleted'})}).catch((err)=>{res.status(400).send({message: error.message})});
+})
 
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 //  Your code goes here
-
+const mario = new mongoose.Schema({
+    name:String,
+    weight:Number
+  });
+  
+  const marioModel = mongoose.model("marioModel", mario);
 
 module.exports = marioModel;
